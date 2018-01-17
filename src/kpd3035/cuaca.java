6cb8ca78c06bb8ccc7eff16585636ebb7fbf5824
ICTKVPJB <ICTKVPJB@ICTKVPJB-PC>
2018-01-17 09:18:56 +0800

    project

---
+++ src/kpd3035/cuaca.java
@@ -0,0 +1,15 @@
+class cuaca
+{
+public static void main(String args[])
+{
+String Lokasi="Kota Bahru";
+double suhu=28.6;
+int Kelembapan=78;
+boolean Amaran_Banjir=false;
+System.out.println("Lokasi; "+Lokasi);
+System.out.println("suhu: "+suhu +"c");
+System.out.println("Kelembapan: "+Kelembapan+"%");
+System.out.println("Amaran_Banjir: "+Amaran_Banjir);
+}
+}
+
